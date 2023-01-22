237e4e98127dd26a0e84ddc63e6165578c6f7b81
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2023-01-22 08:39:17 +0530

    updated

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const Blockchain = require("./blockchain");
+const bodyParser = require("body-parser");
+const PubSub = require("./publishsubscribe");
+const request = require("request");
+
+const app = express();
+const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+setTimeout(() => pubsub.broadcastChain(), 1000);
+
+app.use(bodyParser.json());
+app.get("/api/blocks", (req, res) => {
+  res.json(blockchain.chain);
+});
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  pubsub.broadcastChain();
+  res.redirect("/api/blocks");
+});
+
+const synChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("Replace chain on sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT:${PORT}`);
+  synChains();
+});
